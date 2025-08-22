2971358ac1f8a60cd48eee6616f2b3a17a615241
Sam Felton <sf.samfelton@icloud.com>
2025-08-22 20:59:23 +0100

    refactor: Remove dotenv configuration and update database URL property

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config({ path: ".env" });
 
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
